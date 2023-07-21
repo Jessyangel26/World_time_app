733fd1f4bb379f6a1b77831f2709cd94826e5944
Jessyangel26 <jessyangelj@gmail.com>
2023-07-21 11:15:28 +0530

    title upadted

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -60,7 +60,7 @@ class _HomeState extends State<Home> {
                   });
                     },
                     icon: Icon(Icons.edit_location),
-                    label: Text('Edit Location'),
+                    label: Text('Edit Locations'),
                   ),
                   SizedBox(height: 20.0),
                   Row(
